7caf8d36bce5c1d48e71ec45d13102052fc228a5
641i130 <galileoray@gmail.com>
2021-08-02 22:15:50 -0500

    More

---
+++ gitupload.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [$1 == ""]
+then
+	echo "Please give a commit comment."
+else
+	git add .
+	git commit -m $1
+	git push
+fi
+
---
+++ scripts/notes.gd
@@ -0,0 +1,10 @@
+# Guides Followed:
+"https://youtu.be/ZDghWCd_1k8"
+"https://youtu.be/2nk6bJBTtlA"
+"https://github.com/Bozar/GodotRoguelikeTutorial/wiki"
+# Interesting things:
+"https://kenney.nl/assets/rpg-urban-pack"
+"https://kenney.nl/assets/roguelike-modern-city"
+"https://kenney.nl/assets/roguelike-rpg-pack"
+# Sources:
+"https://kenney.nl/assets/micro-roguelike" # TILES
