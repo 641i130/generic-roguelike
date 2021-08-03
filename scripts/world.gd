6b88dc9cd7dac7e2765c3e3e03389bd5e37739a2
641i130 <galileoray@gmail.com>
2021-08-02 23:45:01 -0500

    MoreKeys

--- scripts/world.gd
+++ scripts/world.gd
@@ -2,18 +2,24 @@ extends Node2D
 # WORLD SCRIPT
 
 onready var tileMap = $TileMap
-var borders = Rect2(1, 1, 38, 21)
+var borders = Rect2(1, 1, 32, 18)
 
 
 func _ready():
 	randomize()
 	gen()
 
-
 func gen():
-	var w = Walker.new(Vector2(20,11),borders)
+	var w = Walker.new(Vector2(32,18),borders)
 	var map = w.walk(500)
 	w.queue_free()
 	for location in map:
 		tileMap.set_cellv(location, -1)
 	tileMap.update_bitmask_region(borders.position,borders.end)
+
+func _input(event):
+	"Check for input"
+	if event.is_action("reset"):
+		get_tree().reload_current_scene()
+	if event.is_action_pressed("exit"):
+		get_tree().quit()
